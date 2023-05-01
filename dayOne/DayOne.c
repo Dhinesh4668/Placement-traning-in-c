88186d1b0f585e1691ee8defdeac657d50154a14
Dhinesh4668 <dhinesh4668@gmail.com>
2023-05-01 14:48:24 +0530

    commt

---
+++ dayOne/DayOne.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main() {
+    int a,b,sum;
+    printf("enter the a valude: ");
+    scanf("%d",&a);
+    printf("enter the b valude: ");
+    scanf("%d",&b);
+    sum = a + b;
+    printf("%d",sum);
+    getch();
+}
