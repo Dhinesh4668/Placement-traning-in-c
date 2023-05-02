777e16294bbffae0f81f617a9364282753b028ad
Dhinesh4668 <dhinesh4668@gmail.com>
2023-05-02 10:06:37 +0530

    commot

--- dayTwo/DayTwo-one.c
+++ dayTwo/DayTwo-one.c
@@ -1,1 +1,12 @@
-#
+#include<stdio.h>
+#include<math.h>
+int main() {
+    int a,b;
+    printf("enter the 2 values:");
+    scanf("%d %d",&a,&b);
+    printf("\n Add :",a+b);
+    printf("\n Dev :",a/b);
+    printf("\n Sub :",a-b);
+    printf("\n Mul :",a*b);
+    printf("\n Mod :",a%b);
+}
---
+++ dayTwo/MultiValInput.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main() {
+    char name;
+    float y;
+    printf("Enter the any value :");
+    scanf("%f",&y);
+    printf("%f",y);
+    getch();
+    // single char 
+    return 0;
+}
