783671107e93c8f5319dee7fb2a9b774a24dfcf2
Dhinesh4668 <dhinesh4668@gmail.com>
2023-05-02 10:32:17 +0530

    factorial program in day two

---
+++ dayTwo/Fact.c
@@ -0,0 +1,16 @@
+//
+// Created by dhine on 02-05-2023.
+//
+#include<stdio.h>
+void main() {
+    int i,n,f=1;
+    printf("\nEnter the input:");
+    scanf("%d",&n);
+    i=1;
+    do{
+        f=f*i;
+        i=i+1;
+
+    }while(i<=n);
+        printf("\n The factorial is:%d", f);
+}
