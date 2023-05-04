7496b34c750d6b767c6b6a2a3755b3cb392e8090
Dhinesh4668 <dhinesh4668@gmail.com>
2023-05-04 14:17:15 +0530

    commit

---
+++ dayFoure/SearchingUsingArray.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main(){
+    int arr[100],n,i,flag=0,element,l=0;
+    printf("\n Enter  the size of array: ");
+    scanf("%d",&n);
+    i=0;
+    while(i<n){
+        printf("\n Enter the value: ");
+        scanf("%d",&n[i]);
+        i++;
+    }
+    printf("\n Input element to be searched : ");
+    scanf("%d",&element);
+    for (i = 0; i < n; i++)
+    {
+        /* code */
+        if (a[i]==0)
+        {
+            /* code */
+            flag=1;
+            i=1;
+        }for ( i = 0; i < n; i++) 
+        printf("\n value is ",arr[i]);
+    }
+    
+
+}
